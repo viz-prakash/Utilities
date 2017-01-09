c8755d70eaefeb9d55f6d9c5a5a0ac7a104b2848
Vijay Prakash <vjprakash0@gmail.com>
2017-01-09 16:15:36 -0500

    first commit of utility

---
+++ add_extension.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+def add_extension(directory, extension):
+	files = [ f for f in os.listdir(directory) if os.path.isfile(f)]
+	for file in files:
+		if "." not in file:
+			print "renaming " + file + " to " + file + "." + extension
+			#print file[:-3]
+			#os.rename(file, file[:-3])
+			os.rename(file, file + "." + extension)
+
+def usage():
+	print "Adds passed extension to the files in given directory which already doesn't have an extension"
+	print "Usage : ./{} Directory_name Extenstion_to_add".format(sys.argv[0])
+	exit()
+		
+def main():
+	if len(sys.argv) != 3:
+		usage()
+	if os.path.isfile(sys.argv[1]):
+		print "Please pass a valid directory...\n\n"
+		usage()
+	add_extension(sys.argv[1], sys.argv[2])
+
+main()	
