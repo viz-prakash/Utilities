73e59640d97b17bc685855798ffc59abf2288157
Vijay Prakash <vjprakash0@gmail.com>
2017-02-16 13:21:50 -0500

    updated the script not to copy existing images
    Updated the script to not copy the images which are already present in target directory. It also check for image formats in windows spotlight repository and then copies it.

--- copy_spotlight_wallpaper.py
+++ copy_spotlight_wallpaper.py
@@ -2,6 +2,10 @@ import getpass
 import os
 import sys
 import shutil
+import imghdr
+
+# copy the script to target Directory and run the script with target directory as .
+# or give path to target directory relative to your current directory
 
 def copy_walls(target_dir):
 	current_user = getpass.getuser()
@@ -12,14 +16,24 @@ def copy_walls(target_dir):
 		if "Microsoft.Windows.ContentDeliveryManager" in file:
 			win_content_delivery_dir = file
 	win_spotlight_dir = win_packages_dir + win_content_delivery_dir + "\\LocalState\\Assets\\"
-	walls = [ f for f in os.listdir(win_spotlight_dir) if os.path.isfile( win_spotlight_dir + f)]
+	walls = [ f for f in os.listdir(win_spotlight_dir) if os.path.isfile( win_spotlight_dir + f) ]
 	for file in walls:
-		if os.stat(win_spotlight_dir + file).st_size >= 200000:
-			shutil.copy2(win_spotlight_dir + file , target_dir + file + ".jpg")
-			print "copied " + win_spotlight_dir + file + " to " + target_dir + file + ".jpg"
+		format = imghdr.what(win_spotlight_dir + file)
+		if os.stat(win_spotlight_dir + file).st_size < 200000 :
+			print "not copying {} because size is less than 200000 bytes".format(win_spotlight_dir + file)
+		elif format is None :
+			print "not copying {} because couldn't figure out the image format of file".format(win_spotlight_dir + file)
+		elif os.path.isfile(target_dir + file + "." + format) :
+			print "not copying {} because file with same name and same image format already exist in target directory".format(win_spotlight_dir + file)
+		else:
+			shutil.copy2(win_spotlight_dir + file , target_dir + file + "." + format)
+			print "copied " + win_spotlight_dir + file + " to " + target_dir + file + "." + format
 
 def usage():
-	print "Usage : ./{} Target_Directory_name".format(sys.argv[0])
+	print "Usage : ./{} Target_Directory_Name".format(sys.argv[0])
+	print "\n This script copies Spotlight wallpaper on Windows 10 to you desired directory"
+	print "\n\n\t\tcopy the script to target Directory and run the script with target directory as ."
+	print "\n\t\t or give path to target directory relative to your current directory"
 	exit()
 
 def main():
