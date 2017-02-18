4d0aed36d47a9da0773dc343e8f0e3d72e0fb524
Vijay Prakash <vjprakash0@gmail.com>
2017-02-18 13:33:05 -0500

    Fixed relative path issue

--- copy_spotlight_wallpaper.py
+++ copy_spotlight_wallpaper.py
@@ -3,12 +3,16 @@ import os
 import sys
 import shutil
 import imghdr
+from PIL import Image
 
 # copy the script to target Directory and run the script with target directory as .
 # or give path to target directory relative to your current directory
 
 def copy_walls(target_dir):
 	current_user = getpass.getuser()
+	if not os.path.isdir(target_dir):
+		os.makedirs(target_dir )
+	tablet_target_dir = target_dir + "\\Tablet\\"
 	win_content_deliverydir = ""
 	win_packages_dir = os.environ['SYSTEMDRIVE'] + "\\Users\\" + current_user + "\\AppData\\Local\\Packages\\"
 	files = [f for f in os.listdir(win_packages_dir) if os.path.isdir(win_packages_dir + f)]
@@ -25,9 +29,18 @@ def copy_walls(target_dir):
 			print "not copying {} because couldn't figure out the image format of file".format(win_spotlight_dir + file)
 		elif os.path.isfile(target_dir + file + "." + format) :
 			print "not copying {} because file with same name and same image format already exist in target directory".format(win_spotlight_dir + file)
-		else:
-			shutil.copy2(win_spotlight_dir + file , target_dir + file + "." + format)
-			print "copied " + win_spotlight_dir + file + " to " + target_dir + file + "." + format
+		else:	
+			im = Image.open(win_spotlight_dir + file)
+			width = im.size[0]
+			height = im.size[1]
+			if width < height:
+				if not os.path.isdir(tablet_target_dir):
+					os.makedirs(tablet_target_dir)
+				shutil.copy2(win_spotlight_dir + file , tablet_target_dir + file + "." + format)
+				print "copied " + win_spotlight_dir + file + " to " + tablet_target_dir + file + "." + format
+			else:
+				shutil.copy2(win_spotlight_dir + file , target_dir + file + "." + format)
+				print "copied " + win_spotlight_dir + file + " to " + target_dir + file + "." + format
 
 def usage():
 	print "Usage : ./{} Target_Directory_Name".format(sys.argv[0])
@@ -39,9 +52,11 @@ def usage():
 def main():
 	if len(sys.argv) != 2:
 		usage()
+	'''
 	if not os.path.isdir(sys.argv[1]):
 		print "Please pass a valid directory...\n\n"
 		usage()
-	copy_walls(os.getcwd() + "\\" + sys.argv[1] + "\\")	
+	'''
+	copy_walls( sys.argv[1] + "\\")	
 
 main()
